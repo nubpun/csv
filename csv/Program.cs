19beef5a11dfb0317e77d685889749966efda1c1
Mayorov Maxim <nub.pun@yandex.ru>
2016-11-15 15:30:40 +0500

    complete first task

--- csv/Program.cs
+++ csv/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace csv
     {
         static void Main(string[] args)
         {
+
+            foreach (var item in ReadCsv1("airquality.csv"))
+            {
+                foreach(var column in item)
+                {
+                    Console.Write(column + ' ');
+                }
+                Console.WriteLine();
+            }
+        }
+        static IEnumerable<string[]> ReadCsv1(string path)
+        {    
+            using (var textReader = new StreamReader(path))
+            {
+                while (true)
+                {
+                    var str = textReader.ReadLine();
+                    if (str == null)
+                        yield break;
+                    string[] ar = str.Split(',');
+                    for (int i = 0; i < ar.Length; i++)
+                    {
+                        if (ar[i] == "NA")
+                        {
+                            ar[i] = null;
+                        }
+                    }
+                    yield return ar;
+                }
+            }
+               
         }
     }
 }
